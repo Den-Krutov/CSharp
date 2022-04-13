8b6b633d2f1f8d613ae6d3a36435aaae8ffd1c85
Даниил Федин <nniiyu@yandex.ru>
2022-04-13 22:55:43 +0300

    15 lesson

---
+++ Den.Delegate/Den.Delegate/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Den.Delegate
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public Person(string name) { Name = name; }
+        public void GetOutBed()
+        {
+            Console.WriteLine($"{Name} встал с кровати");
+        }
+        public void GetInBed()
+        {
+            Console.WriteLine($"{Name} лег в кровать");
+        }
+        public void BrushTeeth()
+        {
+            Console.WriteLine($"{Name} почистил зубы");
+        }
+        public void EatBreakfast()
+        {
+            Console.WriteLine($"{Name} позавтракал");
+        }
+        public void EatDinner()
+        {
+            Console.WriteLine($"{Name} поужинал");
+        }
+        public bool DoWork(bool check)
+        {
+            Console.WriteLine($"{Name} поработал");
+            if (check)
+                return true; 
+            else
+                return false;
+        }
+    }
+}
---
+++ Den.Delegate/Den.Delegate/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Den.Delegate
+{
+    internal class Program
+    {
+        delegate void Morning();
+        static void Main(string[] args)
+        {
+            Person person = new Person("Даня");
+            Morning morningPerson = null;
+            morningPerson?.Invoke();
+            morningPerson += person.GetOutBed;
+            morningPerson += person.EatBreakfast;
+            morningPerson += person.BrushTeeth;
+            morningPerson?.Invoke();
+            Predicate<bool> work = new Predicate<bool>(person.DoWork);
+            if (work(true))
+            {
+                Console.WriteLine("Работа завершена");
+                Action eveningPerson = null;
+                eveningPerson += person.EatDinner;
+                eveningPerson += person.BrushTeeth;
+                eveningPerson += person.GetInBed;
+                eveningPerson();
+            }
+            Func<bool,bool> doWork = new Func<bool,bool>(person.DoWork);
+            if (doWork(true))
+            {
+                Console.WriteLine("Работа завершена");
+                Action eveningPerson = null;
+                eveningPerson += person.EatDinner;
+                eveningPerson += person.BrushTeeth;
+                eveningPerson += person.GetInBed;
+                eveningPerson();
+            }
+            Console.ReadKey();
+        }
+    }
+}
